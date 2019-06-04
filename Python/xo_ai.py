8b1477043de6e328c994780cdfd28793f06586c1
Artembbk <39417597+Artembbk@users.noreply.github.com>
2019-06-04 14:20:14 +0300

    start ai_xo

---
+++ Python/xo_ai.py
@@ -0,0 +1,110 @@
+from graph import *
+
+FIELD_S = 300
+SQUARE_S = 100
+X_FIELD = 100
+Y_FIELD = 100
+
+move = 0
+squares = [[0]*3 for i in range(3)]
+
+
+def square(x, y, a):
+    return rectangle(x, y, x+a, y+a)
+
+
+def cross(x, y, a):
+    line(x, y, x+a, y+a)
+    line(x+a, y, x, y+a)
+
+
+def init():
+    penColor("black")
+    for i in range(3):
+        for j in range(3):
+            squares[i][j] = square(X_FIELD + SQUARE_S*j, Y_FIELD + SQUARE_S*i, SQUARE_S)
+    penSize(3)
+
+
+def hit(x, y, obj):
+    x1, y1, x2, y2 = coords(obj)
+    return (x1 <= x <= x2) and (y1 <= y <= y2)
+
+
+def check_win():
+    A = [[0]*3 for i in range(8)]
+    for i in range(3):
+        A[6][i] = squares[i][i]
+        A[7][i] = squares[i][2-i]
+        for j in range(3):
+            A[i][j] = squares[i][j]
+            A[j+3][i] = squares[i][j]
+    for L in A:
+        if L[0] == L[1] == L[2]:
+            return True
+
+
+def find_win(w):
+    A = [[0]*3 for i in range(8)]
+    for i in range(3):
+        A[6][i] = squares[i][i]
+        A[7][i] = squares[i][2-i]
+        for j in range(3):
+            A[i][j] = squares[i][j]
+            A[j+3][i] = squares[i][j]
+    for k in range(len(A)):
+        
+
+
+def finish_game(w):
+    if w == 1:
+        label("Победили крестики!", 100, 60)
+    elif w == 0:
+        label("Победили нолики!", 100, 60)
+    else:
+        label("Ничья!", 100, 60)
+
+    for i in range(3):
+        for j in range(3):
+            if type(squares[i][j]) == int:
+                squares[i][j] = "W"
+
+
+def mouseClick(event):
+    global move
+    for i in range(3):
+        for j in range(3):
+            if type(squares[i][j]) == int:
+                if hit(event.x, event.y, squares[i][j]):
+                    move += 1
+                    x1, y1, x2, y2 = coords(squares[i][j])
+                    penColor("red")
+                    cross(x1 + SQUARE_S//10, y1 + SQUARE_S//10, (SQUARE_S*8)//10)
+                    squares[i][j] = "X"
+
+                    ai_move()
+
+    if check_win() and move % 2 == 1:
+        finish_game(1)
+    elif check_win() and move % 2 == 0:
+        finish_game(0)
+    elif move == 9:
+        finish_game(2)
+
+
+def ai_move():
+    penColor("blue")
+    if move == 1:
+        if i == j == 1:
+            x1, y1, x2, y2 = coords(squares[0][0])
+            circle(x1 + SQUARE_S//2, y1 + SQUARE_S//2, SQUARE_S//2 - SQUARE_S//10)
+            squares[0][0] = "O"
+        else:
+            x1, y1, x2, y2 = coords(squares[1][1])
+            circle(x1 + SQUARE_S//2, y1 + SQUARE_S//2, SQUARE_S//2 - SQUARE_S//10)
+            squares[1][1] = "O"
+    else:
+
+init()
+onMouseClick(mouseClick)
+run()
